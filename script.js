a011ec907238328db32257a97611367a0d175370
Pushpendra6263 <gulludubey123@gmail.com>
2022-10-04 17:32:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+
+
+
+
+function skewer() {
+        var dim = document.querySelector(".slide").getBoundingClientRect();
+    var prev = dim.left;
+
+    document.querySelector("#page2-box").addEventListener("scroll",function(dets){
+        var dim2 = document.querySelector(".slide").getBoundingClientRect();
+        diff = prev-dim2.left;
+        document.querySelectorAll(".slide").forEach(function(allSlide) {
+            allSlide.style.transform = `skewX(${diff*0.5}deg)`
+            prev = dim2.left;
+        })
+    } )
+}
+function slideCircle() {
+    document.querySelectorAll(".slide").forEach(function (slide) {
+        slide.addEventListener("mousemove", function (dets) {
+            var dim = slide.getBoundingClientRect();
+        slide.children[1].style.clipPath = `circle(20% at ${dets.clientX-dim.left}px ${dets.clientY-dim.top}px)`
+        })   
+        slide.addEventListener("mouseleave", function (dets) {
+            var dim = slide.getBoundingClientRect();
+            slide.children[1].style.clipPath = `circle(0% at ${dets.clientX-dim.left}px ${dets.clientY-dim.top}px)`
+        })   
+    })
+}
+slideCircle()
+skewer();
